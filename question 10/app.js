b9438454809754832cec4fee2bcbb9a07cec3b77
dilmurad947 <webdeveloper1234567890001@gmail.com>
2025-11-30 13:15:10 +0500

    question complete

---
+++ question 10/app.js
@@ -0,0 +1,36 @@
+// console.log("hello world");
+
+
+
+// const veg = [
+//     { name: "carrot", price: 20 },
+//     { name: "potato", price: 10 },
+//     { name: "onion", price: 15 },
+// ];
+// const fruit = [
+//     { name: "apple", price: 30 },
+//     { name: "banana", price: 25 },
+//     { name: "orange", price: 20 },
+// ];
+//     const vegitable = document.querySelector("#vegcheck");
+//     const fruitlist = document.querySelector("#fruitcheck");
+//     const h3 = document.querySelector("#h3");
+    
+// function vegfruit() {
+// if (vegitable.checked == true) {
+//     for (let i = 0; i < veg.length; i++) {
+//       console.log(veg[i].name);
+//     h3.innerHTML = "" 
+//     h3.innerHTML += ` ${veg[0].name} : ${veg[0].price} <br>
+//      ${veg[1].name} : ${veg[1].price} <br>
+//       ${veg[2].name} : ${veg[2].price} <br>
+//     `}}
+//     else if  (fruitlist.checked == true) {
+//     for (let i = 0; i < veg.length; i++) {
+//       console.log(veg[i].name);
+//     h3.innerHTML = "" 
+//     h3.innerHTML += ` ${fruit[0].name} : ${fruit[0].price} <br>
+//      ${fruit[1].name} : ${fruit[1].price} <br>
+//       ${fruit[2].name} : ${fruit[2].price} <br>
+//     `}}
+// }
